7f0eb6a542379c28dec17d513820cd94e9cb6b66
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-01-07 20:37:16 -0500

    worked on module

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,12 @@
-console.log('hello practice');
+const knex = require ('knex')
+
+const knexInstance = knex ({
+    client: 'pg',
+    connection: process.env.DB_URL
+})
+
+const q1 = knexInstance('amazong_products').select('*').toQuery()
+const q2 = knexInstance.from('amazong_products').select('*').toQuery()
+
+console.log('q1:', q1)
+console.log('q2:', q2)
